68c48b3d64c7ccc9b81954e3d7ec9078cb96a415
Mohamed Kawia <send2kawia@gmail.com>
2023-04-13 01:31:21 +0300

    shorter time

---
+++ _app/immutable/chunks/1.1d6835a0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_error.svelte.101f9fdd.js";export{t as component};
--- _app/immutable/chunks/1.91fcb788.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_error.svelte.91512d9f.js";export{t as component};
